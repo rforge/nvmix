6278d6c4aa57ff54e36f9fe787a399c2a5d3c693
ehintz <ehintz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-08-02 12:48:00 +0000

    added qnvmix1()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nvmix@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/qnvmix1.R
@@ -0,0 +1,69 @@
+## qnvmix1()
+
+qnvmix1 <- function(u, shift = 0, scale = 1, mix, N = 1e5, ... ){
+  
+  if(u < 0.5){
+    u <- 1 - u
+    lower <- TRUE
+  } else {
+    lower <- FALSE
+  }
+  
+  ## Define the quantile function of the mixing variable:
+  ## (Copy and paste from pnvmix())
+  
+  W <- if(is.character(mix)) { # 'mix' is a character vector specifying supported mixture distributions (utilizing '...')
+    mix <- match.arg(mix, choices = c("constant", "inverse.gamma"))
+    switch(mix,
+           "constant" = {
+             const <- TRUE
+             function(u){
+               return(1)
+             }
+           },
+           "inverse.gamma" = {
+             if(hasArg(df)) df <- list(...)$df else stop("'mix = \"inverse.gamma\"' requires 'df' to be provided.")
+             ## Still allow df = Inf (normal distribution)
+             stopifnot(is.numeric(df), length(df) == 1, df > 0)
+             if(is.finite(df)) {
+               inv.gam <- TRUE
+               df2 <- df / 2
+               meansqrtmix <- sqrt(df) * gamma(df2) / ( sqrt(2) * gamma( (df+1) / 2 ) ) # mean of sqrt(W) in this case, will be used for preconditioning
+               function(u){
+                 return(1 / qgamma(u, shape = df2, rate = df2))
+               }
+             } else {
+               const <- TRUE
+               meansqrtmix <- 1 # mean of sqrt(W) in this case, will be used for preconditioning
+               function(u){
+                 return(1)
+               }
+             }
+           },
+           stop("Currently unsupported 'mix'"))
+  } else if(is.list(mix)) { # 'mix' is a list of the form (<character string>, <parameters>)
+    stopifnot(length(mix) >= 1, is.character(distr <- mix[[1]]))
+    qmix <- paste0("q", distr)
+    if(!existsFunction(qmix))
+      stop("No function named '", qmix, "'.")
+    function(u){
+      return(  do.call(qmix, c(u, mix[-1])))
+    }
+  } else if(is.function(mix)) { # 'mix' is interpreted as the quantile function F_W^- of the mixture distribution F_W of W
+    function(u){
+      return(mix(u, ...))
+    }
+  }
+  
+  ## Get realizations of sqrt(W)  
+  ## Mutliply by sqrt(scale) for standardization
+  mixings <- sqrt(W(qrng::sobol(n = N, d = 1, randomize = TRUE)) * scale)
+  
+  ## Evaluates the cdf of the mixture distribution using QMC. Can use the same realizations of mixings in each run -> faster (like common random numbers)
+  cdf <- function(b){
+    mean( pnorm( (b - shift) / mixings))
+  }
+  
+  q <- uniroot( function(x){cdf(x)-u}, lower = 0, upper = 10^8, extendInt = "upX")$root
+  if(lower) return(-q) else return(q)
+}
