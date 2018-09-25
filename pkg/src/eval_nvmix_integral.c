4c279ecae04044256103573f4617e7cc145c2126
ehintz <ehintz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-09-25 14:08:54 +0000

    TODO in /src done
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nvmix@35 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/eval_nvmix_integral.c
+++ pkg/src/eval_nvmix_integral.c
@@ -16,28 +16,38 @@
  * @param C lower triangular Cholesky factor as vector
  * @param ZERO smallest number x > 0 such that x != 0
  * @param ONE   largest number x < 1 such that x != 1
- * @return mean estimate of E(g(U)) using antithetic variates
+ * @return mean estimate of E(f(U)) using antithetic variates
  * @note a and b can have -/+Inf entries. While pnorm() would give the correct
  *       result, it safes time to check each time if the argument is -/+Inf
  *       and setting the value to 0/1 rather than calling pnorm().
  * @author Erik Hintz, Marius Hofert (polishing)
  */
 double eval_nvmix_integral_c(double *a, double *b, double *U, int n, int q,
-			     double *C, double ZERO, double ONE)
+                             double *C, double ZERO, double ONE)
 {
-    double y [q-1], sqrtmix,  d,  f,  cone,  diff; /* TODO provide brief description for each variable */
-    double ya[q-1], sqrtmixa, da, fa, conea, diffa; /* corresponding antithetic values */
-    double tmp; /* TODO provide brief description for each variable */
-    double mean = 0; /* TODO provide brief description for each variable */
-    int i, j, l; /* TODO provide brief description for each variable */
-
+    double y[q-1], sqrtmix, d, diff, f, scprod;
+    /* y:       vector to save phi^{-1}(dj+uj(ej-dj))  */
+    /* sqrtmix: used to store sqrt( F_w^{-1}(u_0) )  */
+    /* d:       current values of di from the paper  */
+    /* diff:    current values of (ei-di) from the paper  */
+    /* f:       current value of (e1-d1)*(e2-d2)*...*(ei-di) */
+    /* scprod:  scalar product sum C_{ij}y_j */
+    
+    double ya[q-1], sqrtmixa, da, diffa, fa, scproda; /* corresponding antithetic values */
+    
+    
+    double tmp; /* to store temporary values */
+    double mean = 0; /* to store the result */
+    int i, j, l; /* counters for loops */
+    
+    
     /* For each row of U (for access, use U[s,k] = U[k * numrows + s]) */
     for(j=0; j < n; j++){
-
-        /* Grab the realizations of sqrt(mix) = sqrt(W) TODO: correct ('sqrt(W)')? */
+        
+        /* Grab the realizations of sqrt(mix) = sqrt(W) */
         sqrtmix  = U[j];
         sqrtmixa = U[q*n+j];
-
+        
         /* Check if entry of a is -Inf */
         if(a[0] == R_NegInf){
             d  = 0;
@@ -46,7 +56,7 @@ double eval_nvmix_integral_c(double *a, double *b, double *U, int n, int q,
             d  = pnorm(a[0] / (C[0] * sqrtmix),  0, 1, 1, 0);
             da = pnorm(a[0] / (C[0] * sqrtmixa), 0, 1, 1, 0);
         }
-
+        
         /* Check if entry of b is +Inf */
         if(b[0] == R_PosInf){
             diff  = 1 - d;
@@ -55,62 +65,62 @@ double eval_nvmix_integral_c(double *a, double *b, double *U, int n, int q,
             diff  = pnorm(b[0] / (C[0] * sqrtmix),  0, 1, 1, 0) - d;
             diffa = pnorm(b[0] / (C[0] * sqrtmixa), 0, 1, 1, 0) - da;
         }
-
-	/* Go through all q-1 columns (without first and last) */
+        
+        /* Go through all q-1 columns (without first and last) */
         /* For better readability, we start at i = 0 */
         f  = diff;
         fa = diffa;
-	for(i = 0; i < q-1; i++){
-	    /* U[i * n + j] corresponds to U[j,i] in the orginal matrix */
-	    tmp = d + U[(i+1) * n + j] * diff;
-
-	    /* Check if too close to 0 or 1 */
-	    if(tmp < ZERO){
-		tmp = ZERO;
-	    }
-	    if(tmp > ONE){
-		tmp = ONE;
-	    }
-	    y[i] = qnorm(tmp, 0, 1, 1, 0);
-
-	    /* The same for the antithetic value */
-	    tmp = da + (1-U[(i+1) * n + j]) * diffa;
-	    if(tmp < ZERO){
-		tmp = ZERO;
-	    }
-	    if(tmp > ONE){
-		tmp = ONE;
-	    }
+        for(i = 0; i < q-1; i++){
+            /* U[i * n + j] corresponds to U[j,i] in the orginal matrix */
+            tmp = d + U[(i+1) * n + j] * diff;
+            
+            /* Check if too close to 0 or 1 */
+            if(tmp < ZERO){
+                tmp = ZERO;
+            }
+            if(tmp > ONE){
+                tmp = ONE;
+            }
+            y[i] = qnorm(tmp, 0, 1, 1, 0);
+            
+            /* The same for the antithetic value */
+            tmp = da + (1-U[(i+1) * n + j]) * diffa;
+            if(tmp < ZERO){
+                tmp = ZERO;
+            }
+            if(tmp > ONE){
+                tmp = ONE;
+            }
             ya[i] = qnorm(tmp, 0, 1, 1, 0);
-
-	    /* Calculate the scalar product sum C[i,j] y[j] for j = 1:(i-1) */
-	    cone  = 0;
-	    conea = 0;
+            
+            /* Calculate the scalar product sum C[i,j] y[j] for j = 1:(i-1) */
+            scprod  = 0;
+            scproda = 0;
             for(l = 0; l < (i+1); l++){
-		/* C[l * q + i+1] corresponds to C[i+1,l] in the original matrix */
-		cone  += y[l]  * C[l * q + i+1];
-		conea += ya[l] * C[l * q + i+1];
-	    }
-
+                /* C[l * q + i+1] corresponds to C[i+1,l] in the original matrix */
+                scprod  += y[l]  * C[l * q + i+1];
+                scproda += ya[l] * C[l * q + i+1];
+            }
+            
             /* Calculate new d and diff = e-d */
             if(a[i+1] == R_NegInf){
                 d  = 0;
                 da = 0;
             } else {
-                d  = pnorm((a[i+1] / sqrtmix  - cone)  / C[(i+1)*(q+1)], 0, 1, 1, 0);
-                da = pnorm((a[i+1] / sqrtmixa - conea) / C[(i+1)*(q+1)], 0, 1, 1, 0);
+                d  = pnorm((a[i+1] / sqrtmix  - scprod)  / C[(i+1)*(q+1)], 0, 1, 1, 0);
+                da = pnorm((a[i+1] / sqrtmixa - scproda) / C[(i+1)*(q+1)], 0, 1, 1, 0);
             }
             if(b[i+1] == R_PosInf){
                 diff  = 1 - d;
                 diffa = 1 - da;
             } else {
-                diff  = pnorm((b[i+1] / sqrtmix  - cone)  / C[(i+1)*(q+1)], 0, 1, 1, 0) - d;
-                diffa = pnorm((b[i+1] / sqrtmixa - conea) / C[(i+1)*(q+1)], 0, 1, 1, 0) - da;
+                diff  = pnorm((b[i+1] / sqrtmix  - scprod)  / C[(i+1)*(q+1)], 0, 1, 1, 0) - d;
+                diffa = pnorm((b[i+1] / sqrtmixa - scproda) / C[(i+1)*(q+1)], 0, 1, 1, 0) - da;
             }
-	    f  *= diff;
-	    fa *= diffa;
-	}
-	mean += (f+fa)/2;
+            f  *= diff;
+            fa *= diffa;
+        }
+        mean += (f+fa)/2;
     }
     mean = mean/ n;
     return(mean);
@@ -123,10 +133,10 @@ double eval_nvmix_integral_c(double *a, double *b, double *U, int n, int q,
  * @author Erik Hintz, Marius Hofert (polishing)
  */
 SEXP eval_nvmix_integral(SEXP a, SEXP b, SEXP U, SEXP n, SEXP q, SEXP C,
-			 SEXP ZERO, SEXP ONE)
+                         SEXP ZERO, SEXP ONE)
 {
     double res = eval_nvmix_integral_c(REAL(a), REAL(b), REAL(U), INTEGER(n)[0],
-				       INTEGER(q)[0], REAL(C), REAL(ZERO)[0],
-				       REAL(ONE)[0]);
+                                       INTEGER(q)[0], REAL(C), REAL(ZERO)[0],
+                                       REAL(ONE)[0]);
     return ScalarReal(res);
 }
