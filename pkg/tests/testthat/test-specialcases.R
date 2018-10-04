4aaafec29594a3f646ced4ffc6be309508490d8f
ehintz <ehintz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-10-04 15:32:16 +0000

    minor
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nvmix@86 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/testthat/test-specialcases.R
+++ pkg/tests/testthat/test-specialcases.R
@@ -1,7 +1,33 @@
 context("Special Cases")
 
-test_that("pNorm() deals correctly with d = 1, upper = Inf, lower = -Inf", {
-  expect_equal(pnvmix(upper = Inf, lower = -Inf, qmix = "constant"), expect = 1, check.attributes = FALSE)
-}
-)
+## Some inputs that can be used:
+d <- 3
+set.seed(157)
+A <- matrix(runif(d * d), ncol = d)
+P <- cov2cor(A %*% t(A))
+b <-  3 * runif(d) * sqrt(d) # random upper limit
 
+
+test_that("pnvmix() deals correctly with upper = rep(Inf,d), lower = rep(-Inf,d).", {
+  d <- 3
+  expect_equal(pnvmix(upper = rep(Inf, d), qmix = "constant"), expect = 1, check.attributes = FALSE)
+})
+
+
+test_that("pnvmix() throws error when 'scale' not positive definite.", {
+  ## Define singular 'scale' matrix 
+  scale <- matrix(c(1,0,0,
+                    0,1,0,
+                    0,0,0), ncol = 3, byrow = TRUE)
+  expect_error(pnvmix(upper = c(1,2,3), qmix = "constant", scale = scale))
+})
+
+
+test_that("pStudent deals with df = Inf.", {
+  ## Reset seed to get exactly the same results
+  set.seed(1)
+  p.St.dfInf <- pStudent(b, df = Inf, scale = P)
+  set.seed(1)
+  p.Norm <- pNorm(b, scale = P)
+  expect_equal(p.St.dfInf, p.Norm, check.attributes = FALSE)
+})
