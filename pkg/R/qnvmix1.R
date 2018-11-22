cb80a88e04f756115798c9df1808ac18ac8aa361
ehintz <ehintz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-11-22 01:09:22 +0000

    qnvmix1 added.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nvmix@110 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/qnvmix1.R
@@ -0,0 +1,255 @@
+### qnvmix1() ##################################################################
+##'
+##' @param u vector of probabilities 
+##' @param qmix specification of the (mixture) distribution of W. This can be:
+##'        1) a character string specifying a supported distribution (additional
+##'           arguments of this distribution are passed via '...').
+##'        2) a list of length at least one; the first argument specifies
+##'           the base name of an existing distribution which can be sampled
+##'           with prefix "q", the other elements denote additional parameters
+##'           passed to this "rmix" random number generator.
+##'        3) a function being interpreted as the quantile function F_W^-.
+##' @param stored.values matrix with 3 columns of the form [x, F(x), logf(x)] where
+##'           F and logf are the df and log-density of the distribution specified
+##'           in 'qmix'. 
+##'           If provided it will be used to determine starting values for 
+##'           the internal newton proceudure. Only very basic checking is done.        
+##' @param CI.factor see ?pnvmix()
+##' @param n0 size of initial pointset
+##' @param B see ?pnvmix()
+##' @param abstol.cdf abstol to estimate F(x)
+##' @param abstol.logdensity abstol to estimate logf(x) for the internal Newton procedure
+##' @param abstol.newton abstol for newton
+##' @param max.iter.newton max number of iterations for newton *per element*
+##' @param max.iter.rqmc max number of iterations to estimate F(x) and logf(x) *per element*
+##' @param verbose 
+##' @param q.only if TRUE, only quantiles will be returned, ow additional quantites (see return )
+##' @param ... see ?pnvmix()
+##' @return if q.only is TRUE, vector of same length as u with entries F^{-1}(u)
+##'         if q.only is FALSE, a list of 
+##'         - $q: Vector of quantiles
+##'         - $log.density: Vector log-density values at q
+##'         - $computed.values: matrix with 3 columns [x, F(x), logf(x)] 
+##'         - $newton.iterations: Vector, element i gives nb of 
+##'            Newton iterations needed for u[i]
+##' @author Erik Hintz, Marius Hofert and Christiane Lemieux  
+##' @note - If only the quantiles are needed, abstol.logdensity does not need to be as small.
+##'       - *TODO* Maybe do something smart in the tails
+##'       - *TODO* print better warnings if accuracies not reached and give better
+##'       'verbose' argument (as is done in pnvmix, telling the user which u[i] is
+##'       the one that causes problems). 
+##'       - *TODO* Maybe one can make better use of stored values if one changes
+##'       the order in which the quantiles are evaluated. 
+##'       - *TODO* provide rmix option. (=> later, not important)
+##'       - *TODO* Is it worth having another argument, specifying if the actual
+##'       density shall be returned (instead of logdensity) if  q.only = FALSE
+
+
+qnvmix1 <- function(u, qmix, 
+                    stored.values = NULL,
+                    CI.factor = 3.3, n0 = 2^7, B = 8,
+                    abstol.cdf = 1e-4, abstol.logdensity = 1e-2, abstol.newton = 1e-4,
+                    max.iter.newton = 30, max.iter.rqmc = 15, 
+                    verbose = TRUE, q.only = FALSE, ...) {
+  ## 1 Checking and definitions ################################################
+  
+  ## Basic input checking (stored.values is checked below)
+  stopifnot(!any(u>=1), !any(u<=0), CI.factor >=0, n0 >0, B > 1, abstol.cdf >= 0,
+            abstol.logdensity >=0, abstol.newton >= 0, max.iter.newton > 0,
+            max.iter.rqmc > 0, is.logical(verbose), is.logical(q.only))
+  
+  ## Define the quantile function of the mixing variable: 
+  ## ( Copy & Paste from pnvmix() )
+  is.const.mix <- FALSE # logical indicating whether we have a multivariate normal
+  inv.gam <- FALSE # logical indicating whether we have a multivariate t
+  
+  W <- if(is.character(qmix)) { # 'qmix' is a character vector
+    qmix <- match.arg(qmix, choices = c("constant", "inverse.gamma"))
+    switch(qmix,
+           "constant" = {
+             is.const.mix <- TRUE
+             function(u) 1
+           },
+           "inverse.gamma" = {
+             if(hasArg(df)) {
+               df <- list(...)$df
+             } else {
+               stop("'qmix = \"inverse.gamma\"' requires 'df' to be provided.")
+             }
+             ## Still allow df = Inf (normal distribution)
+             stopifnot(is.numeric(df), length(df) == 1, df > 0)
+             if(is.finite(df)) {
+               inv.gam <- TRUE
+               df2 <- df / 2
+               function(u) 1 / qgamma(1 - u, shape = df2, rate = df2)
+             } else {
+               function(u) 1
+             }
+           },
+           stop("Currently unsupported 'qmix'"))
+  } else if(is.list(qmix)) { # 'mix' is a list of the form (<character string>, <parameters>)
+    stopifnot(length(qmix) >= 1, is.character(distr <- qmix[[1]]))
+    qmix. <- paste0("q", distr)
+    if(!existsFunction(qmix.))
+      stop("No function named '", qmix., "'.")
+    function(u)
+      do.call(qmix., append(list(u), qmix[-1]))
+  } else if(is.function(qmix)) { # 'mix' is the quantile function F_W^- of F_W
+    function(u)
+      qmix(u, ...)
+  } else stop("'qmix' must be a character string, list or quantile function.")
+  
+  ## Special case of normal and t distribution:
+  if(is.const.mix || inv.gam){
+    q <- if(inv.gam) qt(u, df = df) else qnorm(u)
+    if(q.only) return(q) else{
+      l.dens <- if(inv.gam) dt(q, df = df, log = TRUE) else dnorm(q, log = TRUE)
+      return(list(q = q, 
+                  log.density = l.dens, 
+                  computed.values = cbind(q, u, l.dens, deparse.level = 0),
+                  newton.iterations = rep(0, length(u))))
+    }
+  }
+  
+  ## Initialize first pointset needed for rqmc approach:
+  if(!exists(".Random.seed")) runif(1) # dummy to generate .Random.seed
+  seed <- .Random.seed # need to reset to the original seed later 
+  ## Get realizations of W and sqrt(W)
+  mixings <- W(as.vector(sapply(1:B, function(i) qrng::sobol(n0, d = 1, randomize = TRUE)))) #length B*n0
+  sqrt.mixings <- sqrt(mixings)
+  CI.factor <- CI.factor/sqrt(B) # instead of dividing by sqrt(B) all the time
+  
+  ## Function that estimates F(x) and logf(x) for *scalar* input x
+  est.cdf.dens <- function(x){
+    ## Set up result vectors
+    rqmc.estimates.log.density <- rep(NA, B)
+    rqmc.estimates.cdf <- rep(NA, B)
+    ## "realizations" of log density
+    log.dens <- - 1/2 * log(2 * pi * mixings) - x*x / (2*mixings) # length B*no
+    ## "realizations" of cdf
+    cdf <- pnorm(x/sqrt.mixings)
+    ## Grab realizations corresponding to l'th shift and use exp-log trick 
+    for(l in 1:B){
+      log.dens.max <- max( log.dens[ (l-1)*n0 + (1:n0)])
+      rqmc.estimates.log.density[l] <- -log(n0) + log.dens.max + 
+        log(sum(exp(log.dens[ (l-1)*n0 + (1:n0)] - log.dens.max)))
+      rqmc.estimates.cdf[l] <- mean( cdf[(l-1)*n0 + (1:n0)] )
+    }
+    ## Check if precisions are reached
+    error <- c(sd(rqmc.estimates.cdf), sd(rqmc.estimates.log.density))*CI.factor
+    precision.reached <- (error[1] <= abstol.cdf && error[2] <= abstol.logdensity)
+      
+    if(!precision.reached){
+      ## Set up while loop
+      iter.rqmc <- 0
+      current.n <- n0
+      
+      while(!precision.reached && iter.rqmc < max.iter.rqmc){
+        ## Reset seed and get realizations
+        .Random.seed <- seed
+        mixings.new <- W(as.vector(sapply(1:B, function(i) qrng::sobol(current.n, d = 1, randomize = TRUE)))) # length B*current.n
+        ## "realizations" of log density
+        log.dens <- - 1/2 * log(2 * pi * mixings.new) - x*x / (2*mixings.new) 
+        ## "realizations" of cdf
+        cdf <- pnorm(x/sqrt(mixings.new))
+        ## Update RQMC estimators
+        for(l in 1:B){
+          ## Grab realizations corresponding to l'th shift and use exp-log trick 
+          log.dens.max <- max( log.dens[ (l-1)*current.n + (1:current.n)])
+          rqmc.estimates.log.density[l] <- (rqmc.estimates.log.density[l] - 
+            log(current.n) + log.dens.max + 
+            log(sum(exp(log.dens[ (l-1)*current.n + (1:current.n)] - log.dens.max))))/2
+          rqmc.estimates.cdf[l] <- (rqmc.estimates.cdf[l] + 
+            mean( cdf[(l-1)*current.n + (1:current.n)] ) )/2
+        }
+        ## Update sample size, iteration number and error(s)
+        current.n <- 2*current.n
+        iter.rqmc <- iter.rqmc + 1
+        error <- c(sd(rqmc.estimates.cdf), sd(rqmc.estimates.log.density))*CI.factor
+        precision.reached <- (error[1] <= abstol.cdf && error[2] <= abstol.logdensity)
+      }
+    }
+    if(verbose){
+      if(error[1] > abstol.cdf) warning("'abstol.cdf' not reached; consider increasing 'max.iter.rqmc'")
+      if(error[2] > abstol.logdensity) warning("'abstol.logdensity' not reached; consider increasing 'max.iter.rqmc'")
+    }
+    ## Return
+    c(mean(rqmc.estimates.cdf), mean(rqmc.estimates.log.density))
+  }
+  
+  ## Only compute quantiles for u>=0.5 (use symmetry in the other case)
+  lower <- (u < 0.5)
+  u[lower] <- 1 - u[lower]
+  ## Sort u. Ordering needed to return the quantiles in the correct order later
+  ordering <- order(u)
+  u.sorted <- u[ordering]
+  n <- length(u.sorted)
+  ## Build result vectors
+  quantiles <- rep(NA, n)
+  log.density <- rep(NA, n)
+  num.iter.newton <- rep(0, n)
+  
+  if(is.null(stored.values)){
+    ## Matrix of the form [x, F(x), logf(x)] to store df and density evaluations
+    ## First element corresponds to F(x) = 0.5, so that x = 0 
+    ## Note: est.cdf.dens(0)[1] = 0.5 (i.e. no error here)
+    stored.values <- matrix(c(0, est.cdf.dens(0)), nrow = 1)
+  } else {
+    ## Some very basic checking if stored.values was provided
+    stopifnot(is.matrix(stored.values), dim(stored.values)[2] == 3,
+              !any(stored.values[,2] > 1 || stored.values[,2] < 0))
+  }
+  
+  ## 2 Main Loop ###############################################################
+  for (i in 1:n) {
+    ## Initialize error and counter for Newton
+    error <- abstol.newton + 42
+    iter.newton <- 0
+    ## Grab current u
+    current.u <- u.sorted[i]
+    ## Did we already have that u? (could happen if, e.g., u = c(0.4,0.6))
+    if(i > 1 && current.u == u.sorted[i-1]){
+      quantiles[i]   <- quantiles[i-1]
+      log.density[i] <- log.density[i-1]
+      next
+    }
+    ## Get starting value: x in stored.values sth F(x) is close to u
+    closest.ind <- which.min(abs(stored.values[, 2] - current.u))
+    current.qu <- stored.values[closest.ind, 1]
+    current.funvals <- stored.values[closest.ind, 2:3]
+    
+    ## Main loop for Newton procedure
+    while (error > abstol.newton && iter.newton < max.iter.newton) {
+      ## Update quantile:
+      diff.qu <-  current.qu - 
+        (current.qu <- current.qu - sign(current.funvals[1]-current.u)*
+           exp(log( abs(current.funvals[1]-current.u)) - current.funvals[2]))
+      ## Calculate F and logf and store those values
+      current.funvals <- est.cdf.dens(current.qu)
+      stored.values <-  rbind( stored.values, c(current.qu, current.funvals),
+                               deparse.level = 0)
+      ## Update error and increase counter
+      error <- abs(diff.qu)
+      iter.newton <- iter.newton + 1
+    }
+    ## Safe result
+    quantiles[i]       <- current.qu
+    log.density[i]     <- current.funvals[2]
+    num.iter.newton[i] <- iter.newton
+    if(verbose && error > abstol.newton) warning("'abstol.newton' not reached; consider increasing 'max.iter.newton'")
+  }
+  ## Order results according to original ordering of input u
+  quantiles <- quantiles[order(ordering)]
+  ## Use symmetry for those u which were < 0.5
+  quantiles[lower] <- -quantiles[lower]
+  
+  ## Return
+  if(q.only) return(quantiles) else{
+    num.iter.newton <- num.iter.newton[order(ordering)]
+    log.density     <- log.density[order(ordering)]
+    return(list(q = quantiles, 
+                log.density = log.density,
+                computed.values = stored.values,
+                newton.iterations = num.iter.newton))
+  }
+}
---
+++ pkg/man/qnvmix1.rd
@@ -0,0 +1,143 @@
+\name{qnvmix1}
+\alias{qnvmix1}
+\title{Quantile Function of a univariate Normal Variance Mixture Distribution}
+\description{
+  Evaluating multivariate normal variance mixture distribution functions
+  (including normal and Student \emph{t} for non-integer degrees of freedom).
+}
+\usage{
+qnvmix1(u, qmix, 
+        stored.values = NULL,
+        CI.factor = 3.3, n0 = 2^7, B = 8,
+        abstol.cdf = 1e-4, abstol.logdensity = 1e-2, abstol.newton = 1e-4,
+        max.iter.newton = 30, max.iter.rqmc = 15, 
+        verbose = TRUE, q.only = FALSE, ...))
+}
+\arguments{
+  \item{u}{vector of probabilities .}
+  \item{qmix}{specification of the mixing variable \eqn{W}; see McNeil et
+    al. (2015). Supported are the following types of specification (see
+    also the examples below):
+    \describe{
+      \item{\code{\link{character}}:}{a \code{\link{character}} string
+	specifying a supported distribution; currently available are
+        \code{"constant"} (in which case \eqn{W = 1} and thus \code{qnorm} is
+        called) and \code{"inverse.gamma"} (in which case \eqn{W} is an inverse 
+        gamma distribution with shape and rate parameters \code{df}/2 and 
+        \code{qt(..., df = df)} is called; note that \code{df} needs to be provided via
+	 the ellipsis argument then; see the examples below).}
+      \item{\code{\link{list}}:}{a \code{\link{list}} of length at least
+	one, where the first component is a \code{\link{character}}
+	string specifying the base name of a distribution which has a quantile function
+	accessible via prefix \code{"q"}; an example is \code{"exp"}
+        for which \code{"qexp"} exists. If the list is
+        of length larger than one, the remaining elements contain
+        additional parameters of the distribution; for \code{"exp"},
+        this can be the parameter \code{rate}.}
+      \item{\code{\link{function}}:}{a \code{\link{function}}
+	interpreted as the quantile function of the mixing
+	variable \eqn{W}; internally, sampling is then done with the
+	inversion method by applying this function to U(0,1) random variates.}
+    }
+  }
+  \item{stored.values}{\link{matrix} with 3 columns of the form [x, F(x), logf(x)] where
+  F and logf are the df and log-density of the distribution specified
+  in 'qmix'. 
+  If provided it will be used to determine starting values for 
+  the internal newton proceudure. Only very basic checking is done.}
+  \item{CI.factor}{see ?pnvmix().}
+  \item{n0}{size of initial point-set used to internally approximate the df.}
+  \item{abstol.cdf}{abstol to estimate the df F(x) internally. See also ?pnvmix.}
+  \item{abstol.logdensity}{abstol to estimate the log-density logf(x) internally. 
+    See also ?pnvmix.}
+  \item{abstol.newton}{Convergence criterion for the internal Newton method.}
+  \item{max.iter.newton}{Maximum number of iterations for the internal Newton 
+    method for *each* entry of \code{u}}
+  \item{max.iter.rqmc}{Maximum number of iterations of the internal RQMC method
+   to estimate F(x) and logf(x) for each x. }
+  \item{verbose}{\link{logical}, if \code{TRUE} a warning is printed if one of the 'abstol' is
+  not reached.}
+  \item{q.only}{\link{logical}. If \code{TRUE}, only the quantiles are returned; if FALSE, see 
+  Section 'value' below.}
+  \item{\dots}{additional arguments containing parameters of
+    mixing distributions when \code{mix} is a \code{\link{character}}
+    string.}
+}
+\value{
+  If \code{q.only = TRUE} a vector of the same length as \code{u} with entries
+  \eqn{q_i} where \eqn{q_i} satisfies \eqn{q_i = inf_x { F(x) \geq u_}} 
+  where \eqn{F(x)} the univariate df of the normal variance mixture specified 
+  via \code{qmix}; if \code{q.only = FALSE} a list of four:
+  \describe{
+  \item{\code{$q}:}{Vector of quantiles.}
+  \item{\code{$log.density}:}{Vector log-density values at \code{q}}
+  \item{\code{$computed.values}:}{matrix with 3 columns [x, F(x), logf(x)];
+  see details above.}
+  \item{\code{$newton.iterations}:}{Vector giving the number of Newton iterations 
+  needed for \code{u[i]}}}
+}
+\details{
+  This function uses a Newton procedure to estimate the quantile of the specified
+  univariate normal variance mixture distribution. Internally, a randomized 
+  quasi-Monte Carlo (RQMC) approach is used to estimate the distribution and (log)density
+  function; the method is similar to the one in \link{pnvmix} and \link{dnvmix}.
+  The result depends slightly on \code{.random.seed}.
+  
+  Internally, symmetry is used for \eqn{u \le 0.5}. Function values (i.e. cdf and
+  log-density values) are stored and reused to get good starting values. These
+  values are returned if \code{q.only = FALSE} and can be re-used by passing it to
+  \code{qnvmix1} via the argument \code{stored.values}; this can significantly
+  reduce run-time.
+  
+  Accuracy and run-time depend on both the magnitude of \eqn{u} and on how heavy
+  the tail of the underlying distributions is. 
+  Numerical instabilities can occur for values of \eqn{u} close to 0 or 1, especially
+  when the tail of the distribution is heavy. 
+  
+  If \code{q.only = TRUE} the log-density values of the underlying distribution
+  evaluated at the estimated quantiles are returned as well: This can be useful
+  for copula density evaluations where both quantities are needed. 
+  
+}
+}
+\seealso{
+  \code{\link{dnvmix}()}, \code{\link{rnvmix}()}, \code{\link{pnvmix}()}
+}
+\author{Erik Hintz, Marius Hofert and Christiane Lemieux}
+\references{
+  McNeil, A. J., Frey, R., and Embrechts, P. (2015).
+  \emph{Quantitative Risk Management: Concepts, Techniques, Tools}.
+  Princeton University Press.
+}
+\examples{
+### Examples for qnvmix1() ####################################################
+
+## Evaluation points
+u <- seq(from = 0.05, to = 0.95, by = 0.1)
+set.seed(271) # for reproducibility
+
+## Evaluate the t_{1.4} quantile function
+df = 1.4
+qmix. <- function(u) 1/qgamma(u, shape = df/2, rate = df/2)
+## If qmix = "inverse.gamma", qt() is being called 
+qt1  <- qnvmix1(u, qmix = "inverse.gamma", df = df)
+## Estimate quantiles (without using qt())
+qt1. <- qnvmix1(u, qmix = qmix.)
+stopifnot(all.equal(qt1$q, qt1.$q, tolerance = 5e-4))
+## Look at absolute error:
+abs.error <- abs(qt1$q - qt1.$q)
+plot(u, abs.error, type = "l", xlab = "u", ylab = "qt(u)")
+## Now do this again but provide qt1.$stored.values, in which case at most
+## one Newton iteration will be needed:
+qt2 <- qnvmix1(u, qmix = qmix., stored.values = qt1.$computed.values)
+stopifnot(max(qt2$newton.iterations) <= 1)
+
+
+## Evaluate quantile function where W~Exp(2)
+rate = 2
+qexp <- qnvmix1(u, qmix = list("exp", rate = rate))
+## Check: F( F^{-1}(u)) = u 
+stopifnot(all.equal(pnvmix(as.matrix(qexp$q), qmix = list("exp", rate = rate)), u, 
+                    tolerance = 5e-4, check.attributes = FALSE))
+}
+\keyword{distribution}
