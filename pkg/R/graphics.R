2b15eea18ddb83e4a9a10f6b99d8cc9fd5cf7c2f
ehintz <ehintz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-05-05 02:31:47 +0000

    updated references in /man
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nvmix@299 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/graphics.R
+++ pkg/R/graphics.R
@@ -7,6 +7,7 @@
 ##' @param scale see ?pnvmix()
 ##' @param control see ?pnvmix()
 ##' @param plot.diag logical; if TRUE the curve f(x) = x is plotted additionally
+##' @param plot.band logical if CI bands shall be plotted as well 
 ##' @param verbose logical if warnings from underlying 'qgammamix()' shall be
 ##'        thrown
 ##' @param control list of algorithm specific parameters, see ?get_set_param()
@@ -15,8 +16,8 @@
 ##' @return invisibly returns a list of two: 'maha2' (sorted squared mahalanobis
 ##'         distances obtained from 'x', sorted) and 'q' (theoretical quantiles)
 ##' @author Erik Hintz, Marius Hofert, Christiane Lemieux
-qqplot_maha <- function(x, qmix, loc, scale, plot.diag = TRUE, verbose = TRUE,
-                        control = list(), ...)
+qqplot_maha <- function(x, qmix, loc, scale, plot.diag = TRUE, plot.band = TRUE,
+                        verbose = TRUE, control = list(), ...)
 {
    ## Initialize and check inputs
    control <- get_set_param(control)
@@ -28,14 +29,25 @@ qqplot_maha <- function(x, qmix, loc, scale, plot.diag = TRUE, verbose = TRUE,
    d     <- ncol(x)
    ## Obtain sorted Mahalanobis distances (X-loc)^T scale^{-1} (X-loc)
    maha2 <- sort(mahalanobis(x, center = loc, cov = scale))
+   pp <- ppoints(n)
    ## Obtain theoretical quantiles
    theoretical.quantiles <-
-      qgammamix(ppoints(n), qmix = qmix, d = d, control = control,
-                verbose = verbose, q.only = TRUE, stored.values = NULL, ...)
+      qgammamix(pp, qmix = qmix, d = d, control = control,
+                verbose = verbose, q.only = FALSE, stored.values = NULL, ...)
    ## Plot
-   plot(theoretical.quantiles, maha2, xlab = "Theoretical quantiles",
+   plot(theoretical.quantiles$q, maha2, xlab = "Theoretical quantiles",
         ylab = "Sample quantiles", main = "")
    ## Add a diagonal
-   if(plot.diag) lines(theoretical.quantiles, theoretical.quantiles, lty = 2)
+   if(plot.diag) lines(theoretical.quantiles$q, theoretical.quantiles$q, lty = 2)
+   ## Add the CI bands
+   if(plot.band){
+      ## Compute SE of the empirical quantiles (see Fox (2008), pp 35-36)
+      logSE <- (log(pp) + log(1-pp) - log(n))/2 - 
+         theoretical.quantiles$log.density # logarithmic SE 
+      SE <- exp(logSE) 
+      ## Add the band
+      lines(theoretical.quantiles$q, maha2 + 2 * SE, lty = 3)
+      lines(theoretical.quantiles$q, maha2 - 2 * SE, lty = 3)
+   }
    invisible(list(maha2 = maha2, q = theoretical.quantiles))
 }
